6bcfb2e10c68ff28860b259c59d10d6ad5079dc8
Gunnar Dahlberg <gunnarerikdahlberg@gmail.com>
2016-12-04 19:00:33 +0100

    day3 part1

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allLines = ReadAllLines("input.txt").Select(l => l.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            var triangles = ParseTriangles(allLines);
+            var validTriangles = triangles.Where(t => t.IsValid());
+
+            Console.WriteLine($"There are {validTriangles.Count()} valid triangles");
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<Triangle> ParseTriangles(IEnumerable<string[]> inputLines)
+        {
+            var triangles = new List<Triangle>();
+            foreach (var line in inputLines)
+            {
+                var sides = line.Select(l => Int32.Parse(l)).ToArray();
+                var triangle = new Triangle(sides);
+                triangles.Add(triangle);
+            }
+
+            return triangles;
+        }
+
+        private static string[] ReadAllLines(string fileName)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, fileName);
+            var text = File.ReadAllLines(path);
+            return text;
+        }
+    }
+}
---
+++ Day3/Day3/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Triangle
+    {
+        private int A;
+        private int B;
+        private int C;
+
+        public Triangle(int[] sides)
+        {
+            A = sides[0];
+            B = sides[1];
+            C = sides[2];
+        }
+
+        public bool IsValid()
+        {
+            var AB = A + B;
+            var AC = A + C;
+            var BC = B + C;
+
+            return AB > C && AC > B && BC > A;
+        }
+    }
+}
