aad8e7da8ce87cbb563fe1a90bfc7b79a64e3a76
Gunnar Dahlberg <gunnarerikdahlberg@gmail.com>
2016-12-04 20:27:49 +0100

    day4 part 1

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allRooms = ReadAllLines("input.txt").Select(l => new Room(l));
+            var realRooms = allRooms.Where(r => r.IsReal());
+            var sectorIdSum = realRooms.Sum(r => r.SectorId);
+
+            Console.WriteLine($"The sectorId sum is {sectorIdSum}");
+            Console.ReadLine();
+
+        }
+
+        private static string[] ReadAllLines(string fileName)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, fileName);
+            var text = File.ReadAllLines(path);
+            return text;
+        }
+    }
+}
---
+++ Day4/Day4/Room.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Room
+    {
+        private string _input;
+
+        public Room(string input)
+        {
+            _input = input;
+
+        }
+
+        public string CheckSum
+        {
+            get
+            {
+                var indexOfBracket = _input.IndexOf('[');
+                var checksum = _input.Substring(indexOfBracket + 1).TrimEnd(new char[] { ']' });
+                return checksum;
+            }
+        }
+
+        public long SectorId
+        {
+            get
+            {
+                var indexOfDash = _input.LastIndexOf('-');
+                var sectorId = _input.Substring(indexOfDash + 1).Split(new char[] { '[' })[0];
+                return Int64.Parse(sectorId);
+            }
+        }
+
+        public string RoomName
+        {
+            get
+            {
+                var indexOfDash = _input.LastIndexOf('-');
+                var roomName = _input.Substring(0, indexOfDash + 1);
+                return roomName;
+            }
+        }
+
+        private string CalculateChecksum()
+        {
+            var cleanRoomName = RoomName.Replace("-", "");
+            var dictionary = new Dictionary<char, int>();
+            foreach (var c in cleanRoomName)
+            {
+                if (dictionary.ContainsKey(c))
+                    dictionary[c] = dictionary[c] + 1;
+                else
+                    dictionary.Add(c, 1);
+            }
+
+            var ordered = dictionary.OrderByDescending(d => d.Value).ThenBy(d => (int)d.Key);
+            var checksum = new string(ordered.Take(5).Select(d => d.Key).ToArray());
+
+            return checksum;
+        }
+
+
+        public bool IsReal()
+        {
+            var calculatedChecksum = CalculateChecksum();
+            return calculatedChecksum == CheckSum;
+        }
+    }
+}
