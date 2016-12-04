eddff26a37db8614eae8f0dd0fe82edacd1acd7b
Gunnar Dahlberg <gunnarerikdahlberg@gmail.com>
2016-12-04 17:28:36 +0100

    refactor + puzzle part b

---
+++ Day2/Day2/KeyPadSolver.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class KeyPadSolver
+    {
+        private string[,] _keypad;
+
+        public KeyPadSolver(string[,] keypad)
+        {
+            _keypad = keypad;
+        }
+
+        private bool IsValidPosition(Tuple<int, int> position)
+        {
+            if(position.Item1 >= 0 && position.Item2 >= 0)
+            {
+                if(position.Item1 < _keypad.GetLength(0) && position.Item2 < _keypad.GetLength(1))
+                {
+                    var value = _keypad[position.Item1, position.Item2];
+                    if(!String.IsNullOrEmpty(value))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public string GetCodeFromPosition(Tuple<int, int> position)
+        {
+            return _keypad[position.Item1, position.Item2];
+        }
+
+        public Tuple<int, int> GetCodePosition(char[] instructions, Tuple<int, int> startPosition)
+        {
+            var currentPosition = startPosition;
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction == 'L')
+                {
+                    var newPosition = new Tuple<int, int>(currentPosition.Item1, currentPosition.Item2 - 1);
+                    if (IsValidPosition(newPosition))
+                    {
+                        currentPosition = newPosition;
+                    }
+                }
+                if (instruction == 'R')
+                {
+                    var newPosition = new Tuple<int, int>(currentPosition.Item1, currentPosition.Item2 + 1);
+                    if (IsValidPosition(newPosition))
+                    {
+                        currentPosition = newPosition;
+                    }
+                }
+                if (instruction == 'U')
+                {
+                    var newPosition = new Tuple<int, int>(currentPosition.Item1 - 1, currentPosition.Item2);
+                    if (IsValidPosition(newPosition))
+                    {
+                        currentPosition = newPosition;
+                    }
+                }
+                if (instruction == 'D')
+                {
+                    var newPosition = new Tuple<int, int>(currentPosition.Item1 + 1, currentPosition.Item2);
+                    if (IsValidPosition(newPosition))
+                    {
+                        currentPosition = newPosition;
+                    }
+                }
+            }
+
+            return currentPosition;
+        }
+    }
+}
--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -12,95 +12,70 @@ namespace Day2
         static void Main(string[] args)
         {
             var instructions = ReadAllLines("input1.txt");
-            var code = String.Empty;
-            var digit = 5;
 
-            foreach(var instruction in instructions)
-            {
-                digit = GetCodeDigit(instruction.ToArray(), digit);
-                code = String.Concat(code, digit);
-            }
+            var keypadSolver = new KeyPadSolver(GetTraditionalKeyPad());
+            var startPosition = new Tuple<int, int>(1, 1);
+            var traditionalCode = GetCode(keypadSolver, startPosition, instructions);
 
-            Console.WriteLine($"The code is: {code}");
+            keypadSolver = new KeyPadSolver(GetDesignedKeyPad());
+            var currentPosition = new Tuple<int, int>(2, 0);
+            var designCode = GetCode(keypadSolver, currentPosition, instructions);
+
+
+            Console.WriteLine($"The traditional code is: {traditionalCode}");
+            Console.WriteLine($"The design code is: {designCode}");
             Console.ReadLine();
         }
 
-        private static int MinCurrentRow(int currentRow)
+        private static string GetCode(KeyPadSolver keypadSolver, Tuple<int, int> startPosition, string[] instructions)
         {
-            if (currentRow == 1)
-                return 1;
-            if (currentRow == 2)
-                return 4;
-            if (currentRow == 3)
-                return 7;
+            var code = String.Empty;
+            var currentPosition = startPosition;
 
-            throw new NotSupportedException();
-        }
-
-        private static int MaxCurrentRow(int currentRow)
-        {
-            if (currentRow == 1)
-                return 3;
-            if (currentRow == 2)
-                return 6;
-            if (currentRow == 3)
-                return 9;
-
-            throw new NotSupportedException();
-        }
-
-        private static int MinCurrentColumn(int currentColumn)
-        {
-            if (currentColumn == 1)
-                return 1;
-            if (currentColumn == 2)
-                return 2;
-            if (currentColumn == 3)
-                return 3;
-
-            throw new NotSupportedException();
-        }
-
-        private static int MaxCurrentColumn(int currentColumn)
-        {
-            if (currentColumn == 1)
-                return 7;
-            if (currentColumn == 2)
-                return 8;
-            if (currentColumn == 3)
-                return 9;
-
-            throw new NotSupportedException();
-        }
-
-        private static int GetCodeDigit(char[] instructions, int startDigit)
-        {
-            var currentDigit = startDigit;
-
-            foreach(var instruction in instructions)
+            foreach (var instruction in instructions)
             {
-                var currentRow = currentDigit % 3 == 0 ? (currentDigit / 3) : (currentDigit / 3) + 1;
-                var currentColumn = currentDigit % 3 == 0 ? 3 : currentDigit % 3;
-
-                if(instruction == 'L')
-                {
-                    currentDigit = currentDigit - 1 < MinCurrentRow(currentRow) ? MinCurrentRow(currentRow) : currentDigit - 1;
-                }
-                if (instruction == 'R')
-                {
-                    currentDigit = currentDigit + 1 > MaxCurrentRow(currentRow) ? MaxCurrentRow(currentRow) : currentDigit + 1;
-                }
-                if (instruction == 'U')
-                {
-                    currentDigit = currentDigit - 3 < MinCurrentColumn(currentColumn) ? MinCurrentColumn(currentColumn) : currentDigit - 3;
-                }
-                if (instruction == 'D')
-                {
-                    currentDigit = currentDigit + 3 > MaxCurrentColumn(currentColumn) ? MaxCurrentColumn(currentColumn) : currentDigit + 3;
-                }
+                currentPosition = keypadSolver.GetCodePosition(instruction.ToArray(), currentPosition);
+                var instructionCode = keypadSolver.GetCodeFromPosition(currentPosition);
+                code = String.Concat(code, instructionCode);
             }
 
-            return currentDigit;
+            return code;
+        }
+
+        private static string[,] GetDesignedKeyPad()
+        {
+            var keyPad = new string[5, 5];
+            keyPad[0, 2] = "1";
+            keyPad[1, 1] = "2";
+            keyPad[1, 2] = "3";
+            keyPad[1, 3] = "4";
+            keyPad[2, 0] = "5";
+            keyPad[2, 1] = "6";
+            keyPad[2, 2] = "7";
+            keyPad[2, 3] = "8";
+            keyPad[2, 4] = "9";
+            keyPad[3, 1] = "A";
+            keyPad[3, 2] = "B";
+            keyPad[3, 3] = "C";
+            keyPad[4, 2] = "D";
+
+            return keyPad;
+        }
+
+        private static string[,] GetTraditionalKeyPad()
+        {
+            var keyPad = new string[3, 3];
+            keyPad[0, 0] = "1";
+            keyPad[0, 1] = "2";
+            keyPad[0, 2] = "3";
+            keyPad[1, 0] = "4";
+            keyPad[1, 1] = "5";
+            keyPad[1, 2] = "6";
+            keyPad[2, 0] = "7";
+            keyPad[2, 1] = "8";
+            keyPad[2, 2] = "9";
+
+            return keyPad;
         }
 
         private static string[] ReadAllLines(string fileName)
